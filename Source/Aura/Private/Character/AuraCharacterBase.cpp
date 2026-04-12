0cbdf5679d73fcff4b5bedde93d2fa3c244ad927
zhipei-liang <1741838218@qq.com>
2026-04-12 20:05:14 +0800

    创建角色敌人蓝图，绑定角色武器骨骼组件

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -3,9 +3,14 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
