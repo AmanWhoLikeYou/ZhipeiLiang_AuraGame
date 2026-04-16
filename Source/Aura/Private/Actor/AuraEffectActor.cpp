1bb78d0de9b1e535341ff6b94353692db3b49263
zhipei-liang <1741838218@qq.com>
2026-04-16 21:31:48 +0800

    实现火地、药品逻辑完善

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -28,7 +28,7 @@ void AAuraEffectActor::BeginPlay()
 }
 
 void AAuraEffectActor::ApplyGameplayEffectToTarget(AActor* TargetActor,
-	TSubclassOf<UGameplayEffect> GameplayEffectClass) const
+	TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if (TargetASC == nullptr)
@@ -40,5 +40,115 @@ void AAuraEffectActor::ApplyGameplayEffectToTarget(AActor* TargetActor,
 	FGameplayEffectContextHandle EffectContext = TargetASC->MakeEffectContext();
 	EffectContext.AddSourceObject(this);
 	const FGameplayEffectSpecHandle SpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1, EffectContext);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	
+	bool bIsInfinite = SpecHandle.Data->Def->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if (bIsInfinite && InfiniteGameplayEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		ActiveEffectHandles.Add(ActiveEffectHandle, TargetASC);
+	}
+}
+
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+	// 瞬时效果在 BeginOverlap 时应用（若有多个则循环应用）
+	if(InstantGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnBeginOverlap)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GEClass : InstantGameplayEffectClasses)
+		{
+			if (GEClass)
+			{
+				ApplyGameplayEffectToTarget(TargetActor, GEClass);
+			}
+		}
+	}
+
+	// 持续效果在 BeginOverlap 时应用（若有多个则循环应用）
+	if (DurationGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnBeginOverlap)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GEClass : DurationGameplayEffectClasses)
+		{
+			if (GEClass)
+			{
+				ApplyGameplayEffectToTarget(TargetActor, GEClass);
+			}
+		}
+	}
+
+	// 无限效果在 BeginOverlap 时应用（若有多个则循环应用）
+	if (InfiniteGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnBeginOverlap)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GEClass : InfiniteGameplayEffectClasses)
+		{
+			if (GEClass)
+			{
+				ApplyGameplayEffectToTarget(TargetActor, GEClass);
+			}
+		}
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	// 瞬时效果在 EndOverlap 时应用（若有多个则循环应用）
+	if (InstantGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GEClass : InstantGameplayEffectClasses)
+		{
+			if (GEClass)
+			{
+				ApplyGameplayEffectToTarget(TargetActor, GEClass);
+			}
+		}
+	}
+
+	// 持续效果在 EndOverlap 时应用（若有多个则循环应用）
+	if (DurationGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GEClass : DurationGameplayEffectClasses)
+		{
+			if (GEClass)
+			{
+				ApplyGameplayEffectToTarget(TargetActor, GEClass);
+			}
+		}
+	}
+
+	// 无限效果在 EndOverlap 时应用（若有多个则循环应用）
+	if (InfiniteGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GEClass : InfiniteGameplayEffectClasses)
+		{
+			if (GEClass)
+			{
+				ApplyGameplayEffectToTarget(TargetActor, GEClass);
+			}
+		}
+	}
+	
+	if (InfiniteGameplayEffectRemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		
+		TArray<FActiveGameplayEffectHandle> HandlesToRemove;
+		for (const auto& Pair : ActiveEffectHandles)
+		{
+			FActiveGameplayEffectHandle ActiveEffectHandle = Pair.Key;
+			UAbilitySystemComponent* NowASC = Pair.Value;
+			if (NowASC == nullptr)
+			{
+				HandlesToRemove.Add(ActiveEffectHandle);
+				continue;
+			}
+			if (NowASC == TargetASC)
+			{
+				TargetASC->RemoveActiveGameplayEffect(ActiveEffectHandle, 1);
+				HandlesToRemove.Add(ActiveEffectHandle);
+			}
+		}
+		for (const FActiveGameplayEffectHandle& Handle : HandlesToRemove)
+		{
+			ActiveEffectHandles.Remove(Handle);
+		}
+	}
 }
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -3,10 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ActiveGameplayEffectHandle.h"
 #include "GameFramework/Actor.h"
 #include "AuraEffectActor.generated.h"
 
+struct FActiveGameplayEffectHandle;
 class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnBeginOverlap UMETA(DisplayName = "Apply On Begin Overlap"),
+	ApplyOnEndOverlap UMETA(DisplayName = "Apply On End Overlap"),
+	DoNotApply UMETA(DisplayName = "Do Not Apply")
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy : uint8
+{
+	RemoveOnEndOverlap UMETA(DisplayName = "Remove On End Overlap"),
+	DoNotRemove UMETA(DisplayName = "Do Not Remove")
+};
 
 UCLASS()
 class AURA_API AAuraEffectActor : public AActor
@@ -14,19 +31,47 @@ class AURA_API AAuraEffectActor : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+	// 构造函数：设置此 Actor 的默认属性
 	AAuraEffectActor();
 
+
 protected:
-	// Called when the game starts or when spawned
+	// 在游戏开始或该 Actor 被生成时调用
 	virtual void BeginPlay() override;
-	
+
+	// 将指定的 GameplayEffectClass 应用到目标 Actor
 	UFUNCTION(BlueprintCallable)
-	void ApplyGameplayEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass) const;
+	void ApplyGameplayEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	// 当有 Actor 与此 EffectActor 开始重叠时调用（Begin Overlap）
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	// 当有 Actor 与此 EffectActor 结束重叠时调用（End Overlap）
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
-	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	bool bDestroyOnEffectRemoval = false;
 	
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
+	
+	// 瞬时效果：可以配置为多个 GameplayEffect 类
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
-	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+	TArray<TSubclassOf<UGameplayEffect>> InstantGameplayEffectClasses;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	EEffectApplicationPolicy InstantGameplayEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+	
+	// 持续效果：可以配置为多个 GameplayEffect 类
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	TArray<TSubclassOf<UGameplayEffect>> DurationGameplayEffectClasses;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	EEffectApplicationPolicy DurationGameplayEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+	
+	// 永久/无限时长效果：可以配置为多个 GameplayEffect 类
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	TArray<TSubclassOf<UGameplayEffect>> InfiniteGameplayEffectClasses;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	EEffectApplicationPolicy InfiniteGameplayEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	EEffectRemovalPolicy InfiniteGameplayEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
 };
